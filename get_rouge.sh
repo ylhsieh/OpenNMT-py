dd62e23a62d4da6a6452cb5ac9d0daa9d2c807de
ylhsieh <morpheus.h@gmail.com>
2017-10-15 03:39:06 -0500

    Update get_rough to use python version

--- get_rouge.sh
+++ get_rouge.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -x 
-REF=tgt_test.txt
-PRED=pred.txt
+# REF=tgt_test.txt
+# PRED=pred.txt
 # calculate rouge score
-perl ROUGE_with_ranked.pl 1 N $REF $PRED
-perl ROUGE_with_ranked.pl 2 N $REF $PRED R
-perl ROUGE_with_ranked.pl 1 L $REF $PRED
+# perl ROUGE_with_ranked.pl 1 N $REF $PRED
+# perl ROUGE_with_ranked.pl 2 N $REF $PRED R
+# perl ROUGE_with_ranked.pl 1 L $REF $PRED
+python get_python_rouge.py -ans tgt_test.txt -pred pred.txt -source test.txt -output_ranked_r2 ranked_pred.txt
